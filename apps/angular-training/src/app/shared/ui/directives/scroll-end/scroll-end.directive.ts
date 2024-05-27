54a6e3b4b217f3696528c10ebb0892792ea6331f
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2024-05-26 23:59:10 -0500

    feat: Add Scroll End Directive

---
+++ apps/angular-training/src/app/shared/ui/directives/scroll-end/scroll-end.directive.ts
@@ -0,0 +1,98 @@
+import {
+	Directive,
+	ElementRef,
+	NgZone,
+	OnDestroy,
+	afterNextRender,
+	input,
+	output
+} from '@angular/core';
+import { Subscription, fromEvent } from 'rxjs';
+
+@Directive({
+	// eslint-disable-next-line @angular-eslint/directive-selector
+	selector: '[scrollEnd]',
+	standalone: true
+})
+export class ScrollEndDirective implements OnDestroy {
+	nearEnd = output<void>();
+	private subscription: Subscription;
+
+	elementClass = input<string>();
+
+	constructor(
+		private readonly el: ElementRef,
+		private readonly ngZone: NgZone
+	) {
+		afterNextRender(() => {
+			this.runOutSide(() => {
+				const getElement = () => {
+					if (this.elementClass()) {
+						return document?.querySelector(`.${this.elementClass()}`) ?? document;
+					}
+					return document;
+				};
+				const elementMemorized = getElement();
+				const isCustomElement = getElement() instanceof Element;
+
+				this.subscription = fromEvent(elementMemorized, 'scroll', { passive: true }).subscribe(
+					() => {
+						// height of whole window page
+						const heightOfWholePageWindow = window.document.documentElement.scrollHeight;
+
+						const heightOfWholeObserver = isCustomElement
+							? elementMemorized['scrollHeight']
+							: window.document.documentElement.scrollHeight;
+						// how big in pixels the element is
+						const heightOfElement = this.el.nativeElement.scrollHeight;
+
+						// currently scrolled Y position
+
+						const currentScrolledY = isCustomElement
+							? elementMemorized['scrollTop']
+							: window.scrollY;
+
+						// height of opened window - shrinks if console is opened
+						const innerHeight = window.innerHeight;
+
+						/**
+						 * the area between the start of the page and when this element is visible
+						 * in the parent component
+						 */
+						const spaceOfElementAndPage = heightOfWholeObserver - heightOfElement;
+
+						// calculated whether we are near the end
+						const scrollToBottom =
+							heightOfElement - innerHeight - currentScrolledY + spaceOfElementAndPage;
+						const divider = isCustomElement ? 20 : 3;
+						// if the user is near end
+						if (scrollToBottom < heightOfWholePageWindow / divider) {
+							this.runInZone(() => {
+								this.nearEnd.emit();
+							});
+						}
+					}
+				);
+			});
+		});
+	}
+
+	ngOnDestroy(): void {
+		this.runInZone(() => {
+			this.subscription?.unsubscribe?.();
+		});
+	}
+
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	runOutSide(callbak: Function) {
+		this.ngZone.runOutsideAngular(() => {
+			callbak();
+		});
+	}
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	runInZone(callbak: Function) {
+		this.ngZone.run(() => {
+			callbak();
+		});
+	}
+}
